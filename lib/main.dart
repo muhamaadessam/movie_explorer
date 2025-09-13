21452f225cc442793308fae101d43fde91fd2642
muhammadessam <muhammad159e@gmail.com>
2025-09-13 18:09:34 +0300

    edite readme file add screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class MyApp extends StatelessWidget {
         builder: (context, state) {
           return MaterialApp(
             title: 'Movie Explorer',
+            debugShowCheckedModeBanner: false,
             theme: state.themeData,
             themeMode: context.isDarkMode ? ThemeMode.dark : ThemeMode.light,
             home: const MoviesScreen(),
